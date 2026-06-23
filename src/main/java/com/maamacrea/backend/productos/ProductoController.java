ca8b2ce5a5824b7cde6c81f0dd2b0d23904b11e1
Saúl Santibáñez <saulssantis@gmail.com>
2026-06-23 19:38:26 -0400

    Habilita CORS para productos

--- src/main/java/com/maamacrea/backend/productos/ProductoController.java
+++ src/main/java/com/maamacrea/backend/productos/ProductoController.java
@@ -4,6 +4,7 @@ import jakarta.validation.Valid;
 import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/productos")
+@CrossOrigin(origins = "http://localhost:4200")
 public class ProductoController {
 
     private final ProductoService productoService;
